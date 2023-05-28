1bdfba00579c04adc41ba9cf4cc7c29b2c9cb39a
Anil Yadav <anils15787@gmail.com>
2023-05-28 10:10:02 +0530

    updated the test cases

--- ProjTemplateTests/Integration/ProductServiceTests.cs
+++ ProjTemplateTests/Integration/ProductServiceTests.cs
@@ -23,6 +23,7 @@ namespace ProjTemplateTests.Integration
         {
 
         }
+        // This pattern is mix of xUnit , mock and diff reporter which compare the json files stored as approval
         [Fact]
         public async Task GetAllProducts_ReturnList()
         {
@@ -41,6 +42,28 @@ namespace ProjTemplateTests.Integration
 
             //Assert
             Approvals.Verify(JsonConvert.SerializeObject(response,Formatting.Indented));
-        }       
+        }
+        
+        // This test case is purely written using xUnit & mock pattern - latest one and faster
+        [Fact]
+        public async Task GetAllProducts_ReturnList_UsingMock()
+        {
+            //Arrange
+            var productList = new List<Product>()
+            {
+                new Product { Id = 1,ProductName="Product 1",CategoryId=1,IsActive=1,CreatedBy=1,CreatedDate=DateTime.Now,ModifiedBy=1,ModifiedDate=DateTime.Now},
+                new Product { Id = 2,ProductName="Product 2",CategoryId=2,IsActive=1,CreatedBy=1,CreatedDate=DateTime.Now,ModifiedBy=1,ModifiedDate=DateTime.Now}
+            };
+            var productRepositoryMock = _ReplaceWithMock<IProductRepository>();
+            productRepositoryMock.Setup(x => x.GetAllProducts()).ReturnsAsync(productList);
+
+            //Act
+            var productService = _integrationTestFixture.GetApplicationService<IProductService>();
+            var response = await productService.GetAllProducts();
+
+            //Assert
+            Assert.NotNull(response);
+            Assert.Equal(2, response.Count);
+        }
     }
 }
